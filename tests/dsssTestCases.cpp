a0fa50babfcdeef0e99bbe0c56e6194b68b17243
Jacob Brown <jake.brown@redmars.com>
2021-11-11 11:57:58 -0500

    clean up test case.

--- tests/dsssTestCases.cpp
+++ tests/dsssTestCases.cpp
@@ -5,20 +5,23 @@ using namespace std;
 // Test Cases
 int main(){
 	// Main sequence of bits
-	string s = "1001101101010010";
+	string original_sequence = "1001101101010010";
+    string barker_bits = "110";
 	
 	cout << "_______________________________________________________________\n" << endl;
 	
-	cout << "Original Sequence \t: " << s << endl;
+	cout << "Original Sequence \t: " << original_sequence << endl;
 	
 	// Encoded sequence
-	cout << "Encoded \t\t: " << encoder(s, "110") << endl;
+    string encoded_bits = encoder(original_sequence, barker_bits);
+	cout << "Encoded \t\t: " << encoded_bits << endl;
 	
 	// Encoded sequence with 10% error
-	cout << "10% Error applied \t: " << channel(encoder(s, "110"), 10) << endl;
+	cout << "10% Error applied \t: " << channel(encoded_bits, 10) << endl;
 	
 	// Decoded sequence of encoded sequence
-	cout << "Decoded Sequence \t: " << decoder(encoder(s, "110"), "110") << endl;
+    string decoded_bits = decoder(encoded_bits, barker_bits);
+	cout << "Decoded Sequence \t: " << decoded_bits << endl;
 	
 	cout << "_______________________________________________________________" << endl;
 }
